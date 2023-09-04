920ed3e2c131a9f62f558e7289c96cd250948490
ShozoKurie <141247924+ShozoKurie@users.noreply.github.com>
2023-09-04 21:44:11 +0900

    出品によるitemsテーブルへのデータ保存機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
   
   def new
@@ -7,5 +8,18 @@ class ItemsController < ApplicationController
   end
   
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to :index
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  private
+
+  def item_params
+  params.require(:item).permit(:item_name, :caption, :category_id, :condition_id, :postage_id, :region_id, :days_to_ship_id, :price, :image).merge(hoge: fuga).merge(user_id: current_user.id)
+  end
+
 end
