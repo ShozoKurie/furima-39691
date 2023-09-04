c8e16041dfd72b7127bb9bc2ddfc437c5217d99a
ShozoKurie <141247924+ShozoKurie@users.noreply.github.com>
2023-09-04 16:09:59 +0900

    itemsテーブルのマイグレーションファイルの編集とマイグレート

--- db/migrate/20230904065728_create_items.rb
+++ db/migrate/20230904065728_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[7.0]
   def change
     create_table :items do |t|
-
+      t.string     :item_name,             null: false
+      t.text       :caption,               null: false
+      t.integer    :category_id,           null: false
+      t.integer    :condition_id,          null: false
+      t.integer    :postage_id,            null: false
+      t.integer    :region_id,             null: false
+      t.integer    :days_to_ship_id,       null: false
+      t.integer    :price,                 null: false
+      t.references :user,                  null: false, foreign_key: true
       t.timestamps
     end
   end
