282ce4aa8ed8a029719ae322662c0f53bc7ed5ee
ShozoKurie <141247924+ShozoKurie@users.noreply.github.com>
2023-09-04 17:58:04 +0900

    ActiveHashに関するバリデーションの追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,16 @@
 class Item < ApplicationRecord
-  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  has_one :region
+
+
   validates :item_name, presence: true
   validates :caption, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :postage_id, presence: true
-  validates :region_id, presence: true
-  validates :days_to_ship_id, presence: true
-  validates :price, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :postage_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :region_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :days_to_ship_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, presence: true,numericality: { greater_than_or_equal_to: 300}
   validates :user, presence: true
 
 
