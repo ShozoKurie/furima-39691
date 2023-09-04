6a7ef600ea18c1806c54af5eab0aaa405f62b058
ShozoKurie <141247924+ShozoKurie@users.noreply.github.com>
2023-09-04 16:36:02 +0900

    itemモデルのバリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  
+  validates :item_name, presence: true
+  validates :caption, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :postage_id, presence: true
+  validates :region_id, presence: true
+  validates :days_to_ship_id, presence: true
+  validates :price, presence: true
+  validates :user, presence: true
+
+
 end
