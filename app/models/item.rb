0a83cd0d9a8c31343a3b5d0fe336da7f7e5bf8ec
ShozoKurie <141247924+ShozoKurie@users.noreply.github.com>
2023-09-04 18:39:30 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   has_one :region
   belongs_to :category
   has_one :condition
-  
+
 
   validates :item_name, presence: true
   validates :caption, presence: true
@@ -12,7 +12,7 @@ class Item < ApplicationRecord
   validates :postage_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :region_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :days_to_ship_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price, presence: true,numericality: { greater_than_or_equal_to: 300}
+  validates :price, presence: true,numericality: {  in: 300..9999999 }
   validates :user, presence: true
 
 
