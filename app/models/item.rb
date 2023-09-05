69253cefc22fdb475a10d548d32fe5048eb3dabf
ShozoKurie <141247924+ShozoKurie@users.noreply.github.com>
2023-09-05 20:31:50 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  has_one :region
+  belongs_to :region
   belongs_to :category
-  has_one :condition
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :days_to_ship
   has_one_attached :image
   belongs_to :user
 
@@ -14,8 +16,7 @@ class Item < ApplicationRecord
   validates :postage_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :region_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :days_to_ship_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price, presence: true,numericality: { in: 300..9999999, message: "must be in 300~9999999 by Single-byte numeric value" } 
-  validates :user, presence: true
+  validates :price, presence: true,numericality: { only_integer: true, in: 300..9999999, message: "must be in 300~9999999 by Single-byte numeric value" } 
   validates :image, presence: true  
 
 
