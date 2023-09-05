4376e7d21d5d49b193061206e0e4dc599e4428d2
ShozoKurie <141247924+ShozoKurie@users.noreply.github.com>
2023-09-05 15:06:42 +0900

    手数料と利益の数値表示の処理

---
+++ app/javascript/new.js
@@ -0,0 +1,20 @@
+function culculate() {
+  const price = document.getElementById("item-price")
+  const fee = document.getElementById("add-tax-price")
+  const profit = document.getElementById("profit")
+
+  price.addEventListener('input', function(){
+    if(isNaN(price.value)){
+      fee.textContent = "isNaN"
+      profit.textContent = "isNaN"
+    } else {
+      const feeValue = Math.floor(price.value * 0.1)
+      const profitValue = price.value - feeValue
+      fee.textContent = Math.floor(feeValue)
+      profit.textContent = Math.floor(profitValue)
+    }
+  })
+    
+}
+
+window.addEventListener('load', culculate)
