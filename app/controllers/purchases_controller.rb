eb562e4bad089f371cc6c9e3f1f8a6926118da59
ShozoKurie <141247924+ShozoKurie@users.noreply.github.com>
2023-09-10 20:27:12 +0900

    予期しないユーザーのアクションを適切なビューへリダイレクト

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,13 +1,13 @@
 class PurchasesController < ApplicationController
-  
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
+  before_action :shut_item_owner, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     @purchase_destination = PurchaseDestination.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @purchase_destination = PurchaseDestination.new(purchase_params)
     if @purchase_destination.valid?
        @purchase_destination.save
@@ -22,4 +22,15 @@ class PurchasesController < ApplicationController
   def purchase_params
     params.require(:purchase_destination).permit(:post_code, :region_id, :city, :street_address, :building, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def shut_item_owner
+    if current_user == @item.user
+      redirect_to root_path
+    end
+  end
+
 end
