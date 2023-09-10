d6bee52229d52a052a7e1c34507a51973aaa9756
ShozoKurie <141247924+ShozoKurie@users.noreply.github.com>
2023-09-10 19:53:59 +0900

    バリデーションの修正

--- app/models/purchase_destination.rb
+++ app/models/purchase_destination.rb
@@ -4,7 +4,7 @@ class PurchaseDestination
 
   with_options presence: true do
     validates :post_code
-    validates :region_id
+    validates :region_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :city
     validates :street_address
     validates :phone_number
