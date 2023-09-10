9c5ce8f426ca80c19e65c32013d82a65a4e2a610
ShozoKurie <141247924+ShozoKurie@users.noreply.github.com>
2023-09-10 18:09:08 +0900

    購入コントローラーで購入テーブルと配送先テーブルへの保存機能の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,23 @@
 class PurchasesController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @purchase_destination = PurchaseDestination.new
+  end
+
+  def create
+    
+    @purchase_destination = PurchaseDestination.new(purchase_params)
+    if @purchase_destination.valid?
+       @purchase_destination.save
+       redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_destination).permit(:post_code, :region_id, :city, :street_address, :building, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
   end
 end
