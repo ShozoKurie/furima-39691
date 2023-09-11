6276e5e2246f5e13b9bcd3b6987ea589b5c76500
ShozoKurie <141247924+ShozoKurie@users.noreply.github.com>
2023-09-11 16:36:03 +0900

    購入に関するバリデーションの修正

--- app/models/purchase_destination.rb
+++ app/models/purchase_destination.rb
@@ -3,11 +3,13 @@ class PurchaseDestination
   attr_accessor :item_id, :user_id, :purchase_id, :post_code, :region_id, :city, :street_address, :building, :phone_number, :token
 
   with_options presence: true do
-    validates :post_code
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :street_address
-    validates :phone_number
+    validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. Include hyphen(-)"}
     validates :token
+    validates :item_id
+    validates :user_id
   end
   validates :region_id, numericality: { other_than: 1 , message: "can't be blank"}
 
